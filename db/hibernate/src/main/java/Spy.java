5325a7bdbeec7ccc783fe9825452df96ae1e39e9
Martin Reder <martin.reder@hotmail.com>
2021-04-15 13:36:48 +0200

    Create hibernate folder + java classes + hibernate configuration

---
+++ db/hibernate/src/main/java/Spy.java
@@ -0,0 +1,17 @@
+public class Spy {
+    private String      codeName;
+    private String      name;
+    private Mission[]   missions;
+    private boolean     retired;
+    private boolean     alive;
+
+    public Spy() {};
+
+    public String       getCodeName()   {return codeName;};
+    public String       getName()       {return name;};
+    public Mission[]    getMissions()   {return missions;};
+    public boolean      getRetired()    {return retired;};
+    public boolean      getAlive()      {return alive;};
+}
+
+/*TODO- Setters and Constructor*/
