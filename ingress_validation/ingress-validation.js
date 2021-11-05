4baf4773e7ea83d8deb74a4df9a6a45be8a9f671
Martin Reder <martin.reder@hotmail.com>
2021-11-05 15:11:39 +0100

    add ingress-validation

--- ingress_validation/ingress-validation.js
+++ ingress_validation/ingress-validation.js
@@ -8,26 +8,6 @@ module.exports = function (app) {
     app.get('/ingress-validation', function (req, res) {
         //basic command execution - result will be in stdout
         try {
-            // const command = `if [ -f "ingress.yaml" ]; then
-            //                     $DEFAULTBACKEND_PORT=$(cat ingress.yaml | yq e '.spec.defaultBackend.service.port.number' -  )
-            //                     $RULES_PORT=$(cat ingress.yaml | yq e '.spec.rules[0].http.paths[0].backend.service.port.number' -)
-            //                     if [$RULES_PORT = null]; then
-            //                         $PORT = $DEFAULTBACKEND_PORT
-            //                     else
-            //                         $PORT = $RULES_PORT
-            //                     fi
-            //                     cat ingress_validation/ingress-echo.service.yaml | yq e '.spec.ports[].port = $PORT' - | sudo sponge ingress-echo.service.yaml
-            //                     kubectl apply -f ingress_validation/ingress-echo.deployment.yaml
-	        //                     kubectl apply -f ingress_validation/ingress-echo.service.yaml
-            //                     wait
-            //                     cat ingress.yaml | yq e '.spec.defaultBackend.service.name = "echoserver"' - | sudo sponge ingress.yaml
-            //                     cat ingress.yaml | yq e '.spec.rules[].http.paths[].backend.service.name = "echoserver"' - | sudo sponge ingress.yaml 
-            //                     kubectl apply -f ingress.yaml
-            //                     wait
-            //                     curl http://challenge.test/?echo_body=funktioniert -L
-            //                 else 
-            //                     echo "ingress.yaml does not exist!"
-            //                 fi`;
             const command = `if [ -f "ingress.yaml" ]; then
                                 $DEFAULTBACKEND_PORT=$(cat ingress.yaml | yq e '.spec.defaultBackend.service.port.number' -  )
                                 $RULES_PORT=$(cat ingress.yaml | yq e '.spec.rules[0].http.paths[0].backend.service.port.number' -)
@@ -36,8 +16,15 @@ module.exports = function (app) {
                                 else
                                     $PORT = $RULES_PORT
                                 fi
-                                set -x $DEFAULTBACKEND_PORT
-                                set -x $RULES_PORT
+                                cat ingress_validation/ingress-echo.service.yaml | yq e '.spec.ports[].port = "$PORT"' - | sudo sponge ingress-echo.service.yaml
+                                kubectl apply -f ingress_validation/ingress-echo.deployment.yaml
+	                            kubectl apply -f ingress_validation/ingress-echo.service.yaml
+                                wait
+                                cat ingress.yaml | yq e '.spec.defaultBackend.service.name = "echoserver"' - | sudo sponge ingress.yaml
+                                cat ingress.yaml | yq e '.spec.rules[].http.paths[].backend.service.name = "echoserver"' - | sudo sponge ingress.yaml 
+                                kubectl apply -f ingress.yaml
+                                wait
+                                curl http://challenge.test/?echo_body=funktioniert -L
                             else 
                                 echo "ingress.yaml does not exist!"
                             fi`;
