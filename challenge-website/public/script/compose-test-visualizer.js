70358e9ca7d822224555ee712fdbd211f6a2eac7
Alexander Eisl <alexander.eisl@gmx.at>
2021-12-02 10:29:20 +0100

    change website structure to have tests seperated into pages

--- challenge-website/public/script/compose-test-visualizer.js
+++ challenge-website/public/script/compose-test-visualizer.js
@@ -1,5 +1,61 @@
-import { TestCase } from './test-case-library.js';
+import { TestCase } from './test-case-library.js'
 import { statusCodes } from './test-case-library.js';
+import { changePage } from './test-case-library.js';
+import { getActivePage } from './test-case-library.js';
+import { resetPage } from './test-case-library.js';
+
+const prevBtn = document.getElementById("previous");
+const nextBtn = document.getElementById("next");
+prevBtn.addEventListener("click", prevPage);
+nextBtn.addEventListener("click", nextPage);
+//set manually to highest page count
+let maxPage = 3;
+
+//update button availability
+function updateButtons() {
+    if (getActivePage() <= 1) {
+        prevBtn.setAttribute("disabled", true);
+    } else {
+        prevBtn.removeAttribute("disabled");
+    }
+
+    if (getActivePage() >= maxPage) {
+        nextBtn.setAttribute("disabled", true);
+    } else {
+        nextBtn.removeAttribute("disabled");
+    }
+}
+
+function prevPage() {
+    if (getActivePage() > 1) {
+        changePage(-1);
+        resetPage();
+        updateButtons();
+        renderAll();
+    }
+}
+
+function nextPage() {
+    if (getActivePage() < maxPage) {
+        changePage(+1);
+        resetPage();
+        updateButtons();
+        renderAll();
+    }
+}
+
+function renderAll() {
+    for (let testCase of testCases) {
+        testCase.render()
+    }
+}
+
+//execute all tests
+async function runTests() {
+    for (let testCase of testCases) {
+        await testCase.execute();
+    }
+}
 
 //button runs all tests
 const runTestsBtn = document.getElementById("run-tests");
@@ -69,16 +125,10 @@ let testCase3 = async function () {
 
 //test case declarations
 let testCases = [];
-testCases.push(new TestCase(1, "Test Case 1: API running", false, testCase1));
-testCases.push(new TestCase(2, "Test Case 2: API <-> Database connection", false, testCase2));
-testCases.push(new TestCase(3, "Test Case 3: https Upgrade", false, testCase3));
+testCases.push(new TestCase(1, 1, "Test Case 1: API running", "<p>Willkommen zu Ihrer ersten Mission, starten wir mit einem kleinen Training im docker-compose File:</p><p>Ihre erste Aufgabe besteht darin, den Java API Server erreichbar zu machen, stellen Sie dafür beim docker-compose File den richtigen Backend Port für die API ein, diesen finden Sie in der haproxy.cfg (ha-proxy/haproxy.cfg) und machen Sie diesen von dem Port 80 von außen erreichbar. (<a href=\"https://www.haproxy.com/de/blog/the-four-essential-sections-of-an-haproxy-configuration/\">Link zur Dokumentation</a>)</p><p>Zum Testen rufen sie localhost/missions mit einem Browser auf. Der Aufruf sollte ein leeres Ergebnis enthalten.</p>", false, testCase1));
+testCases.push(new TestCase(2, 2, "Test Case 2: API <-> Database connection", "", false, testCase2));
+testCases.push(new TestCase(3, 3, "Test Case 3: https Upgrade", "", false, testCase3));
 
 //initial render of tests
-testCases.forEach(element => element.render());
+renderAll();
 
-//execute all tests
-async function runTests() {
-    for (let testCase of testCases) {
-        await testCase.execute();
-    }
-}
--- challenge-website/public/script/kubernetes-test-visualizer.js
+++ challenge-website/public/script/kubernetes-test-visualizer.js
@@ -1,5 +1,61 @@
 import { TestCase } from './test-case-library.js'
 import { statusCodes } from './test-case-library.js';
+import { changePage } from './test-case-library.js';
+import { getActivePage } from './test-case-library.js';
+import { resetPage } from './test-case-library.js';
+
+const prevBtn = document.getElementById("previous");
+const nextBtn = document.getElementById("next");
+prevBtn.addEventListener("click", prevPage);
+nextBtn.addEventListener("click", nextPage);
+//set manually to highest page count
+let maxPage = 3;
+
+//update button availability
+function updateButtons() {
+    if (getActivePage() <= 1) {
+        prevBtn.setAttribute("disabled", true);
+    } else {
+        prevBtn.removeAttribute("disabled");
+    }
+
+    if (getActivePage() >= maxPage) {
+        nextBtn.setAttribute("disabled", true);
+    } else {
+        nextBtn.removeAttribute("disabled");
+    }
+}
+
+function prevPage() {
+    if (getActivePage() > 1) {
+        changePage(-1);
+        resetPage();
+        updateButtons();
+        renderAll();
+    }
+}
+
+function nextPage() {
+    if (getActivePage() < maxPage) {
+        changePage(+1);
+        resetPage();
+        updateButtons();
+        renderAll();
+    }
+}
+
+function renderAll() {
+    for (let testCase of testCases) {
+        testCase.render()
+    }
+}
+
+//execute all tests
+async function runTests() {
+    for (let testCase of testCases) {
+        await testCase.execute();
+    }
+}
 
 //button runs all tests
 const runTestsBtn = document.getElementById("run-tests");
@@ -63,22 +119,23 @@ let securityTest = async function () {
     return result;
 }
 
-
 //test case declarations
 let testCases = [];
-testCases.push(new TestCase(1, "Kubernetes database", true, async () =>
-    fetch('http://localhost:3000/validate-kubernetes-database')
-        .then(response => {
-            if (response.status != 200)
-                return [false, "Database does not work as expected."]
-
-            return [true, "Database persists Agents during a restart!"];
-        })
-        .catch(reason => [false, reason])
-));
-testCases.push(new TestCase(2, "API Test", true, apiTest));
-testCases.push(new TestCase(3, "Security Test - test if secret is correctly configured", true, securityTest))
-testCases.push(new TestCase(4, "Kubernetes ingress", true, async () =>
+testCases.push(new TestCase(1, 1, "API Test", "", true, apiTest));
+testCases.push(new TestCase(1, 2, "Security Test - test if secret is correctly configured",
+`
+<p>Nicht nur der Ingress konnte nicht mehr erreicht werden, sondern auch die API wurde in Mitleidenschaft gezogen.</p>
+<p>Bei dem Versuch die Schnittstelle zu erreichen wird immer ein 503 Error Service Temporarily unavailable geliefert.</p>
+<p>Um den Fehler zu beheben müssen Sie einen API Service erstellen, welcher über den Port 8080 erreichbar ist. <a href="https://kubernetes.io/docs/concepts/services-networking/service/">Link zur Dokumentation</a></p>
+<p>Der Doppelagent hat außerdem Sicherheitslücken in unseren Konfigurationen hinterlassen. Um unser System sicherer zu machen, sollten Sie unsere Applikation 
+mit Datenbank credentials erweitern. <a href="https://kubernetes.io/docs/tasks/inject-data-application/distribute-credentials-secure/">Link zur Dokumentation</a></p>
+`, true, securityTest))
+testCases.push(new TestCase(2, 3, "Kubernetes ingress",
+`
+<p>Jeder Versuch sich mit dem Ingress zu verbinden scheitert, konfigurieren Sie die ingress.yaml Datei so, dass es wieder möglich ist sich mit dem Host challenge.test über den API Service den Sie zuvor konfiguriert haben zu verbinden.
+<a href="https://kubernetes.io/docs/concepts/services-networking/ingress/">Link zur Dokumentation</a></p>
+<p>Außerdem soll ein https Upgrade durchgeführt werden. <a href="https://kubernetes.github.io/ingress-nginx/user-guide/nginx-configuration/annotations/#server-side-https-enforcement-through-redirect">Link zur Dokumentation</a></p>
+`, true, async () =>
     fetch('http://localhost:3000/ingress-validation')
         .then(response => {
             if (response.status != 200)
@@ -88,13 +145,22 @@ testCases.push(new TestCase(4, "Kubernetes ingress", true, async () =>
         })
         .catch(reason => [false, reason])
 ))
+testCases.push(new TestCase(3, 4, "Kubernetes database",
+`
+<p>Ausgezeichnet! Unsere Applikation funktioniert wieder und unsere Agenten können Ihre Informationen wieder sicher und verlässlich teilen.</p>
+<p>Allerdings gibt es noch ein Problem: Mehrere Nutzer haben berichtet, dass Ihre Daten nach einem Wartungsfenster verschwunden sind. Sorgen Sie dafür,
+dass bei einem Neustart der Datenbank keine Daten mehr verloren gehen! Ergänzen Sie dazu das Datenbank Deployment um einen Volume Claim.
+<a href="https://kubernetes.io/docs/concepts/storage/persistent-volumes/">Link zur Dokumentation</a></p>
+`, true, async () =>
+    fetch('http://localhost:3000/validate-kubernetes-database')
+        .then(response => {
+            if (response.status != 200)
+                return [false, "Database does not work as expected."]
+
+            return [true, "Database persists Agents during a restart!"];
+        })
+        .catch(reason => [false, reason])
+));
 
 //initial render of tests
-testCases.forEach(element => element.render());
-
-//execute all tests
-async function runTests() {
-    for (let testCase of testCases) {
-        await testCase.execute();
-    }
-}
+renderAll();
