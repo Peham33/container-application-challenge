167537ffe7aa1efbf3055e70326ca3e0dcd040ce
Andreas Pramendorfer <1praand1@gmail.com>
2021-12-03 13:55:15 +0100

    Add error messages to api and database validation

--- challenge-website/validate-kubernetes-database.js
+++ challenge-website/validate-kubernetes-database.js
@@ -1,6 +1,5 @@
 const childProcess = require("child_process");
-
-module.exports  = app => {
+module.exports = app => {
     app.get('/validate-kubernetes-database', (req, response) => {
         const id = Date.now().valueOf() % 1000;
         const registerCurl = `curl -m 5 -d 'codeName=${id}&name=test' https://challenge.test/register -H 'content-type: application/x-www-form-urlencoded' -ik`;
@@ -8,50 +7,37 @@ module.exports  = app => {
         const restartPod = `kubectl scale deployment database --replicas=0; sleep 1; kubectl scale deployment database --replicas=1`
         const waitForDatabase = `timeout 15s bash -c -- 'while [[ $(${loginCurl} | grep "HTTP/2 504" | wc -l) -eq 1  ]]; do ${loginCurl}; sleep 0.5; done'`
 
-        const body = {};
-        let html = "";
+        const body = {
+            success: false,
+            tests: [
+                { message: 'API is reachable.', success: false },
+                { message: "Can create an agent via the API.", success: false },
+                { message: "Can use created agent for login.", success: false },
+                { message: "Data is preserved after restart.", success: false }
+            ]
+        };
 
         try {
-            let stdout = childProcess.execSync(registerCurl, {encoding: 'utf-8'})
-            body["registerCurl"] = stdout;
-            html += `<h1>Initial Create</h1>`;
-            html += `<code>${stdout}</code>`;
-            const registerSuccess = (stdout.match(/HTTP\/.*201/) || []).length === 1;
-            html += `<p>${registerSuccess}</p>`;
-            body["registerSuccess"] = registerSuccess;
+            let stdout = childProcess.execSync(registerCurl, { encoding: 'utf-8' })
+            body.tests[1].success = (stdout.match(/HTTP\/.*201/) || []).length === 1;
 
-            stdout = childProcess.execSync(loginCurl, {encoding: 'utf-8'})
-            body["loginCurl"] = stdout;
-            html += `<h1>Login</h1>`;
-            html += `<code>${stdout}</code>`;
-            const loginSuccess = (stdout.match(/HTTP\/.*200/) || []).length === 1;
-            html += `<p>${loginSuccess}</p>`;
-            body["loginSuccess"] = loginSuccess;
+            stdout = childProcess.execSync(loginCurl, { encoding: 'utf-8' })
+            body.tests[2].success = (stdout.match(/HTTP\/.*200/) || []).length === 1;
 
-            stdout = childProcess.execSync(restartPod, {encoding: 'utf-8'})
-            body["restartPod"] = stdout;
-            stdout = childProcess.execSync(waitForDatabase, {encoding: 'utf-8', shell: 'bash'})
-            body["waitForDatabase"] = stdout;
-            html += `<h1>Wait for Database</h1>`;
-            html += `<code>${stdout}</code>`;
-
-            stdout = childProcess.execSync(loginCurl, {encoding: 'utf-8'})
-            body["loginAfterRestart"] = stdout;
-            html += `<h1>Login 2</h1>`;
-            html += `<code>${stdout}</code>`;
-            const loginAfterRestartSuccess = (stdout.match(/HTTP\/.*200/) || []).length === 1;
-            html += `<p>${loginAfterRestartSuccess}</p>`;
-            body["loginSuccess"] = loginAfterRestartSuccess;
-
-            body["html"] = html.toString();
+            stdout = childProcess.execSync(restartPod, { encoding: 'utf-8' })
+            stdout = childProcess.execSync(waitForDatabase, { encoding: 'utf-8', shell: 'bash' })
+            stdout = childProcess.execSync(loginCurl, { encoding: 'utf-8' })
+            body.tests[3].success = (stdout.match(/HTTP\/.*200/) || []).length === 1;
 
             response.status(
-                loginAfterRestartSuccess ? 200 : 500
+                body.tests[2].success ? 200 : 500
             );
+            body.success = body.tests[2].success;
             response.json(body);
-        } catch(error) {
+        } catch (error) {
             response.status(500);
-            response.json(error);
+            body.tests[0].success = false;
+            response.json(body);
         }
     })
 }
